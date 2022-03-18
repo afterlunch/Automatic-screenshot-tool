db7530665454befb1a6f8900fe2af417e19b72d2
afterlunch <95327249+afterlunch@users.noreply.github.com>
2022-03-18 14:36:16 +0800

    Update jietu.py

--- jietu.py
+++ jietu.py
@@ -9,8 +9,8 @@ import logging
 import os
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-secret_id = 'AKID4OXR6WgaEdmfLlwrXx72i8IEuMYUAdrp'     # 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-secret_key = 'kFUUEixwf8MK3Ypqc8RWLuDH5dvylJbp'   # 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+secret_id = 'AKID4OXR6WaEdmfLlwrXx72i8IEuMYUAdrp'     # 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+secret_key = 'kFUUEixw8MK3Ypqc8RWLuDH5dylJbp'   # 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
 region = 'ap-nanjing'      # 替换为用户的 region，已创建桶归属的region可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
                            # COS支持的所有region列表参见https://cloud.tencent.com/document/product/436/6224
 token = None               # 如果使用永久密钥不需要填入token，如果使用临时密钥需要填入，临时密钥生成和使用指引参见https://cloud.tencent.com/document/product/436/14048  # 指定使用 http/https 协议来访问 COS，默认为 https，可不填
@@ -18,7 +18,7 @@ scheme = 'https'
 config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token, Scheme=scheme)
 client = CosS3Client(config)
 #proxies = {
- #   'http': 'proxy.huawei.com:8080',  # 替换为用户的 HTTP代理地址
+ #   'http': '',  # 替换为用户的 HTTP代理地址
 #}
 ##client = CosS3Client(config)
 #创建文件夹
